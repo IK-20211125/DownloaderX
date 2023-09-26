eb8face0b5ec2bc4ff9c08a55ca293b8805fa7a9
IK <95004425+IK-20211125@users.noreply.github.com>
2023-09-26 10:46:42 +0900

    Add files via upload

---
+++ DownloaderX.py
@@ -0,0 +1,157 @@
+# GuIライブラリ
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+# YouTube動画ダウンロードライブラリ
+import yt_dlp
+
+# OSライブラリ
+import os
+
+# マルチスレッド処理ライブラリ
+import threading
+
+
+def main():
+    # ウィンドウを作成
+    window = tk.Tk()
+    
+    # 左上のアイコン変更
+    # iconfile = './cat.ico'
+    # window.iconbitmap(default=iconfile)
+    
+    # ウィンドウのタイトルを設定
+    window.title("DownloaderX")
+
+    # ウィンドウのサイズを設定
+    window.geometry("400x300")
+
+    # ラベル1 を作成して配置
+    label1 = tk.Label(window, text="URL")
+    # レイアウト調整
+    label1.pack(pady=10, padx=10)
+
+    # URLテキスト入力欄
+    entry_url = tk.Entry(width=40)
+    # レイアウト調整
+    entry_url.pack(pady=10, padx=10)
+
+    # ラベル2 を作成して配置
+    label2 = tk.Label(window, text="Save Location")
+    # レイアウト調整
+    label2.pack(pady=10, padx=10)
+
+    # PATHテキスト入力欄
+    entry_path = tk.Entry(width=40)
+    # レイアウト調整
+    entry_path.pack(pady=10, padx=10)
+
+    # ラジオボタンを配置
+    choice = tk.IntVar()
+    # 初期値に動画を設定
+    choice.set(1)
+    # ラジオボタンを横に配置
+    frame = tk.Frame(window)
+    frame.pack(pady=10)
+    # mp4で出力
+    radio_button_mp4 = tk.Radiobutton(frame, text="Movie", variable=choice, value=1)
+    radio_button_mp4.pack(side=tk.LEFT, padx=10, pady=10)
+    # mp3で出力
+    radio_button_mp3 = tk.Radiobutton(frame, text="Music", variable=choice, value=2)
+    radio_button_mp3.pack(side=tk.LEFT, padx=10, pady=10)
+
+    # ボタンがクリックされたときの処理
+    def download_button_clicked():
+        # プログレスバー作成
+        pb = progressbar_set(window)
+        # プログレスバー開始
+        pb.start(7)
+
+        # URL取得
+        url = entry_url.get()
+
+        # ラジオボタン値取得
+        flag = choice.get()
+
+        # PATH取得
+        path = entry_path.get()
+
+        # PATH指定されない場合、Videosフォルダ配下にYTDLファルダ作成
+        if path == "":
+            path = (
+                os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + "\\Videos" + "\\YTDL"
+            )
+
+        # 別スレッドで動画のダウンロードを実行
+        thread_download = threading.Thread(
+            target=download_video, args=(url, path, flag, pb)
+        )
+        # ダウンロードスレッド開始
+        thread_download.start()
+
+    # ボタンを作成して配置
+    button = tk.Button(window, text="Download", command=download_button_clicked)
+    # レイアウト調整
+    button.pack(pady=10, padx=10)
+
+    # イベントループを開始
+    window.mainloop()
+
+
+# プログレスバーを作成する関数
+def progressbar_set(window):
+    
+    # プログレスバーのスタイルを設定
+    style = ttk.Style()
+    style.theme_use("default")
+    style.configure(
+        "base.Horizontal.TProgressbar",
+        thickness=17,  # プログレスバーの幅
+        troughcolor="#d0d0d0",  # プログレスバーの背景色
+        background="#008000",  # プログレスバーの色
+    )
+
+    # プログレスバーを作成
+    bar = tk.IntVar()
+    # 作成
+    pb = ttk.Progressbar(
+        window,
+        style="base.Horizontal.TProgressbar",
+        maximum=100,
+        mode="indeterminate",
+        variable=bar,
+        length=200,
+    )
+    # 設置
+    pb.pack(pady=10, padx=10)
+    
+    return pb
+
+
+# 指定したURL上の動画をダウンロードする関数
+def download_video(url, path, flag, pb):
+    
+    # オプション設定
+    ydl_opts = {
+        "outtmpl": os.path.join(path, f'%(title)s{"" if flag == 2 else ".mp4"}'),
+        "format": "bestaudio" if flag == 2 else "best[ext=mp4]",
+        "postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}] if flag == 2 else [],
+    }
+
+    # ダウンロード実行
+    try:
+        # ダウンロード
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        # プログレスバー破壊
+        pb.destroy()
+    except Exception as e:
+        # プログレスバー破壊
+        pb.destroy()
+        # エラー表示
+        messagebox.showerror("Error", "Download failed.")
+
+
+
+if __name__ == "__main__":
+    main()
